7c1c8c2d9f9c1d71009c0d60e1c32caa3849d092
Keishon Smith <keishon104@gmail.com>
2016-09-30 16:42:32 -0400

    Re-Designed Program
    While working in a copy of this program. I descovered "string.length"
    and also "stringCharAt()" function that'll allow me to read each
    individual character.

--- Morse/Morse.ino
+++ Morse/Morse.ino
@@ -3,18 +3,15 @@
  * Purpose: To input a message and output the same message through morse code. 
  */
 
-
+int j = 1;                                               // 
+int i;                                                   //
 int WhitePin ;                                           // Declaring the pin that LED is connected to
 String StartPin = "What pin is your LED connected to?";  // Message the serial communication port asks on startup
 String Connect = "Your LED is conected to pin: ";        // Asks what Pin the LED is connected to
-String Message;                                          // Declaring message that'll be translated 
-String com;
-int j = 1;
-int i;
-int blinks;
+String Message = "Place Holder" ;                        // Declaring message that'll be translated 
+
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(WhitePin, OUTPUT);
   int A = a();                                             //Initializing morse code for "A"
@@ -22,28 +19,48 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:  
   for(j; j<=1; j++){
     Serial.println(StartPin);
     while(Serial.available()==0){ }
     WhitePin = Serial.parseInt();                          // What number you enter becomes the pin that the command will output to 
     Serial.print(Connect), Serial.println(WhitePin);       // Displays text of what pin LED its connected to
   }
+  
   Serial.println("Input your message");                    // Ask what the message is 
   while(Serial.available()==0){}                           // Waits for message input 
   Message = Serial.readString();                           // Message that will be translated 
-  
-  Serial.print("How many times do you want the led to blink?");
-  while(Serial.available()==0){};
-  blinks = Serial.parseInt();
-  for(i=0; i<=blinks; i++){
-    b();
+
+   for ( i=0; i<= Message.length(); i++){                  // Repeat printing the message for message length 
+    si(i);
+    calling(i);
+    if(Message.charAt(i)== 'W'){                           // Checks if the individual character is certain letter. 
+      Serial.println("This is a W and begin to finish this project");      
+    }
   }
   
   delay(1000);
-  
+
 }
 
+void si(char x){                                           // This function repeats the words verifying its correctly working. 
+    Serial.println((Message[i]));
+    if(x == 'O' || x == 'o'){
+      Serial.print("This is an O");
+    }
+    delay(200);
+}
+void calling( char x){                                  // This function calls certain functions when needed 
+  switch (Message.charAt(x)) {
+    case 'O': 
+        Serial.println("The test works");
+      break;
+     case' ':
+      Serial.println("This is a space");
+    default:
+        Serial.println();
+      break;
+  }
+}
 
 
 int a(){
@@ -65,3 +82,14 @@ int b(){
   delay(2000);
 }
 
+
+void A(){
+  digitalWrite(13,HIGH);
+  delay(250);
+  digitalWrite(13,LOW);
+  delay(250);
+  digitalWrite(13,HIGH);
+  delay(500);
+  digitalWrite(13,LOW);
+  delay(1000);
+};
